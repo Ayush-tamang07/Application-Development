6f06583ab9158a35bcd0dbe22e267f007236ce5b
Ayush Tamang <tamangayush052@gmail.com>
2024-09-24 09:17:41 +0545

    done sum, used main method, take input from user

---
+++ Week-1/workshop/Program.cs
@@ -0,0 +1,48 @@
+﻿// Using System
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Console.WriteLine("Hello World");
+        // Ask for student Id
+        // Console.Write("Enter your london met Id:");
+        // string studentId = Console.ReadLine();
+
+        // Ask for student name
+        // Console.Write("Enter your name:");
+        // string studentName = Console.ReadLine();
+
+        // Ask for student's age
+        // Console.Write("Enter your age:");
+        // int studentAge = Convert.ToInt32(Console.ReadLine());
+
+        // Ask for gender
+        // Console.Write("Enter your gender:");
+        // string studentGender = Console.ReadLine();
+
+        // Print the student details
+        // Console.WriteLine("\nStudent details:");
+        // Console.WriteLine("Id:" + studentId);
+        // Console.WriteLine("Name:" + studentName);
+        // Console.WriteLine("Age:" + studentAge);
+        // Console.WriteLine("Gender:" + studentGender);
+
+        // adding two numbers
+        // Console.WriteLine("Enter first number:");
+        // int num1 = Convert.ToInt32(Console.ReadLine());
+        // Console.WriteLine("Enter second number:");
+        // int num2 = Convert.ToInt32(Console.ReadLine());
+        // var sum = num1+num2;
+        // Console.WriteLine("\nSum:"+ sum );
+
+        
+
+
+
+
+    }
+}
+
+
+
