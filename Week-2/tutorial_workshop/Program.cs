7d34de29eeb0e19c87b8a739cac11eccbd15bffe
Ayush Tamang <tamangayush052@gmail.com>
2024-10-01 08:40:44 +0545

    unirary operator

---
+++ Week-2/tutorial_workshop/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq.Expressions;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Unary operators
+        // var x = 5;
+
+        // Console.WriteLine("post increment:");
+        // int postIncrement = x++;
+        // Console.WriteLine(postIncrement);
+        // Console.WriteLine(x);
+
+        // Console.WriteLine("pre increment:");
+        // int preIncrement = ++x;
+        // Console.WriteLine(preIncrement);
+        // Console.WriteLine(x);
+
+        // Console.WriteLine("post decrement:");
+        // int postDecrement = x--;
+        // Console.WriteLine(postDecrement);
+        // Console.WriteLine(x);
+
+        // Console.WriteLine("pre decrement:");
+        // int preDecrement = --x;
+        // Console.WriteLine(preDecrement);
+        // Console.WriteLine(x);
+
+    }
+
+}
+
