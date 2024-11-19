f066c0d2374676990b62d6503d287454d2e5f4ca
Ayush Tamang <tamangayush052@gmail.com>
2024-11-19 08:48:14 +0545

    doing workshop task

---
+++ week-3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // var obj = new Program();
+        string fullName = GetFullName("Ayush", "Ayush");
+        Console.WriteLine(fullName);
+
+        // string fullName = obj.GetFullName("Ayush", "Ayush");
+        // Console.WriteLine(fullName);
+    }
+
+    static String GetFullName(string firstName, string lastName)
+    {
+        // fullName = ($"{firstName}{lastName}");
+        if (lastName == "")
+        {
+            return firstName;
+        }
+        if (firstName == "")
+        {
+            return lastName;
+        }
+        var fullName = $"{firstName} {lastName}";
+        return fullName;
+
+    }
+}
